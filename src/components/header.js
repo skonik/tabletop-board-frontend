b8c8828db1ea11aabcc0c68112873b81e21ba97e
Skonik <s.konik.job@gmail.com>
2021-12-23 21:47:09 +0300

    Add body

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,9 @@
 
 export default function Header() {
     return (
-        <div className="header" style={{background: "#28293D", }}>
+        <div className="header" >
 
-            <nav className="py-2 md:py-4">
+            <nav className="py-2 md:py-4" style={{background: "#28293D", }}>
                 <div className="container px-4 mx-auto md:flex md:items-center">
 
                     <div className="flex justify-between items-center">
@@ -20,15 +20,33 @@ export default function Header() {
                         <a href="#"
                            className="p-2 lg:px-4 md:mx-2 text-white rounded hover:bg-gray-200 hover:text-gray-700 transition-colors duration-300">Find Game 🎲</a>
                         <a href="#"
-                           className="p-2 lg:px-4 md:mx-2 text-white rounded hover:bg-gray-200 hover:text-gray-700 transition-colors duration-300">About</a>
+                           className="p-2 lg:px-4 md:mx-2 text-white rounded hover:bg-gray-200 hover:text-gray-700 transition-colors duration-300">About 🎩</a>
                         <a href="#"
                            className="p-2 lg:px-4 md:mx-2 text-white text-center border border-transparent rounded hover:bg-indigo-100 hover:text-indigo-700 transition-colors duration-300">Login</a>
                         <a href="#"
                            className="p-2 lg:px-4 md:mx-2 text-white text-center border border-solid border-indigo-600 rounded hover:bg-indigo-600 hover:text-white transition-colors duration-300 mt-1 md:mt-0 md:ml-1">Signup</a>
                     </div>
+
                 </div>
+
             </nav>
 
+            <div className="py-6 md:py-12" style={{background: "#1C1C27"}}>
+                <div className="container px-4 mx-auto">
+
+                    <div className="text-center max-w-2xl mx-auto">
+                        <h1 className="text-3xl md:text-2xl text-white font-medium mb-2">Find players for your tabletop game and meet new friends
+                            </h1>
+                        <button className="bg-indigo-600 text-white py-2 px-6 rounded-full text-xl mt-6">Get Started
+                        </button>
+                    </div>
+
+
+                </div>
+            </div>
+
         </div>
+
     )
 }
+
